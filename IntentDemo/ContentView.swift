608fde1f98cd04bd433f034523dcec9892449f3e
Philipp Meyer <philipp-meyer-freital@t-online.de>
2024-04-10 14:15:56 +0200

    Initial Commit

---
+++ IntentDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntentDemo
+//
+//  Created by Philipp Meyer on 10.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntentDemo/IntentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentDemoApp.swift
+//  IntentDemo
+//
+//  Created by Philipp Meyer on 10.04.24.
+//
+
+import SwiftUI
+
+@main
+struct IntentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
